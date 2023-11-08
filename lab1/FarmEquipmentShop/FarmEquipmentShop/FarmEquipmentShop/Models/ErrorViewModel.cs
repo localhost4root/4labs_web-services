85a97547a82777feabb1d3abe7900e2af55e2871
unknown <Localhost>
2023-11-08 21:53:50 +0200

    Lab3 clean up cache

--- lab1/FarmEquipmentShop/FarmEquipmentShop/FarmEquipmentShop/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace FarmEquipmentShop.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
